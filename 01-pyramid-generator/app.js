574872ad6e08e3ea0dd4c75e1261469c7dff9a46
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-05-18 22:26:41 +0500

    first project pyramid generator

---
+++ 01-pyramid-generator/app.js
@@ -0,0 +1,15 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1));
+}
+console.log(rows);
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
